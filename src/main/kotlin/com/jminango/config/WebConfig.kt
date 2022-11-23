9fdaac64fe05b485920b150d78c37fb84daf85b2
jminango20 <jcarlosminango@gmail.com>
2022-11-23 10:15:54 -0300

    content negotiation via Header Parameter

--- src/main/kotlin/com/jminango/config/WebConfig.kt
+++ src/main/kotlin/com/jminango/config/WebConfig.kt
@@ -9,9 +9,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class WebConfig : WebMvcConfigurer{
 
     override fun configureContentNegotiation(configurer: ContentNegotiationConfigurer) {
-        configurer.favorParameter(true)
-            .parameterName("mediaType") //come from Postman
-            .ignoreAcceptHeader(true)
+//        configurer.favorParameter(true)
+//            .parameterName("mediaType") //come from Postman
+//            .ignoreAcceptHeader(true)
+//            .useRegisteredExtensionsOnly(false)
+//            .defaultContentType(MediaType.APPLICATION_JSON)
+//                .mediaType("json", MediaType.APPLICATION_JSON)
+//                .mediaType("xml", MediaType.APPLICATION_XML)
+
+        configurer.favorParameter(false)
+            .ignoreAcceptHeader(false)
             .useRegisteredExtensionsOnly(false)
             .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("json", MediaType.APPLICATION_JSON)
