fdada7c56718a9b80aeec8d53a9bfb6b13ccf13f
jminango20 <jcarlosminango@gmail.com>
2022-11-23 11:03:09 -0300

    serialization with YAML refactoring by creating mediaType object

--- src/main/kotlin/com/jminango/controller/PersonController.kt
+++ src/main/kotlin/com/jminango/controller/PersonController.kt
@@ -3,8 +3,9 @@ package com.jminango.controller
 import com.jminango.data.vo.v1.PersonVO
 import com.jminango.data.vo.v2.PersonVO as PersonVOV2
 import com.jminango.services.PersonService
+import com.jminango.utils.MediaType
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.MediaType
+
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
@@ -16,30 +17,30 @@ class PersonController {
     @Autowired
     private lateinit var personService : PersonService
 
-    @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, "application/x-yaml"])
+    @GetMapping(produces = [MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML])
     fun findAll() : List<PersonVO>{
         return personService.findAll()
     }
-    @GetMapping(value = ["/{id}"], produces = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, "application/x-yaml"])
+    @GetMapping(value = ["/{id}"], produces = [MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML])
     fun findById(@PathVariable(value = "id") id : Long) : PersonVO {
         return personService.findById(id)
     }
 
-    @PostMapping(produces = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, "application/x-yaml"], consumes = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, "application/x-yaml"])
+    @PostMapping(produces = [MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML], consumes = [MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML])
     fun createPerson(@RequestBody person: PersonVO) : PersonVO {
         return personService.createPerson(person)
 
-    }    @PostMapping(value = ["/v2"], produces = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, "application/x-yaml"], consumes = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, "application/x-yaml"])
+    }    @PostMapping(value = ["/v2"], produces = [MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML], consumes = [MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML])
     fun createPersonV2(@RequestBody person: PersonVOV2) : PersonVOV2 {
         return personService.createPersonV2(person)
     }
 
-    @PutMapping(produces = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, "application/x-yaml"], consumes = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, "application/x-yaml"])
+    @PutMapping(produces = [MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML], consumes = [MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML])
     fun updatePerson(@RequestBody person: PersonVO) : PersonVO {
         return personService.updatePerson(person)
     }
 
-    @DeleteMapping(value = ["/{id}"], produces = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE])
+    @DeleteMapping(value = ["/{id}"], produces = [MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML])
     fun deletePerson(@PathVariable(value = "id") id : Long) : ResponseEntity<*> {
         personService.deletePerson(id)
         return ResponseEntity.noContent().build<Any>()
