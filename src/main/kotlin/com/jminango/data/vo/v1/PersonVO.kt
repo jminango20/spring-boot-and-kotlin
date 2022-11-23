2b49f8eb7d74828badb39587ec890a7919f86f16
jminango20 <jcarlosminango@gmail.com>
2022-11-22 21:04:40 -0300

    Json serialization to change the name of field, ignore, change the order

--- src/main/kotlin/com/jminango/data/vo/v1/PersonVO.kt
+++ src/main/kotlin/com/jminango/data/vo/v1/PersonVO.kt
@@ -1,9 +1,21 @@
 package com.jminango.data.vo.v1
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
+
+@JsonPropertyOrder("id", "address", "first_name", "last_name", "gender")
 data class PersonVO(
     var id : Long = 0L,
+
+    @field:JsonProperty("first_name")
     var firstName : String = "",
+
+    @field:JsonProperty("last_name")
     var lastName : String = "",
+
     var address : String = "",
+
+    @JsonIgnore
     var gender : String = ""
 )
