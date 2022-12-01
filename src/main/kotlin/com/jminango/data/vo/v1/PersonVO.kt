5bc8c3ace9ddb417ab0cd5bbae42e3f10ece737d
jminango20 <jcarlosminango@gmail.com>
2022-12-01 08:24:19 -0300

    Adding swagger to the project

--- src/main/kotlin/com/jminango/data/vo/v1/PersonVO.kt
+++ src/main/kotlin/com/jminango/data/vo/v1/PersonVO.kt
@@ -1,10 +1,14 @@
 package com.jminango.data.vo.v1
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
 import com.github.dozermapper.core.Mapping
 import org.springframework.hateoas.RepresentationModel
 
+@JsonPropertyOrder("id", "firstName", "lastName", "address", "gender")
 data class PersonVO(
     @Mapping("id")
+    @field:JsonProperty("id")
     var key : Long = 0L,
     var firstName : String = "",
     var lastName : String = "",
